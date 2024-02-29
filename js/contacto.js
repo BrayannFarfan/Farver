fe5501f49832f732a74d243b595a893281260d59
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2024-02-28 22:57:32 -0300

    agregando validaciones al form

--- js/contacto.js
+++ js/contacto.js
@@ -1,5 +1,9 @@
 
 const form = document.getElementById('contactForm');
+const container_bottom = document.getElementById('container_bottom');
+const loader = document.getElementById('loader');
+const button = document.getElementById('button');
+const contac_send = document.getElementById('contac_send');
 
 
 form.addEventListener('submit', handleSubmitContact);
@@ -7,20 +11,36 @@ form.addEventListener('submit', handleSubmitContact);
 async function handleSubmitContact(e){
     e.preventDefault();
 
+    button.disabled = true;
+
+    container_bottom.classList.add('oculto')
+    loader.innerText = 'Cargando...'
     const data = new FormData(this);
-
-    const res =await fetch(this.action, {
-        method: this.method,
-        body: data,
-        headers:{
-            'Accept':'application/json'
+    
+    try {
+        const res =await fetch(this.action, {
+            method: this.method,
+            body: data,
+            headers:{
+                'Accept':'application/json'
+            }
+        })
+        
+        if(res.ok){
+            this.reset();
+            container_bottom.classList.remove('oculto')
+            loader.innerText = ''
+            contac_send.classList.add('exito');
+            contac_send.innerText = 'Gracias por contartarme, te escribiré pronto' 
         }
-    })
-
-    if(res.ok){
-        this.reset();
-        alert('Gracias por contartarme, te escribiré pronto')
+        
+    } catch (error) {
+        
+    } finally {
+        contac_send.innerText = '' 
+        button.disabled = false;
     }
+
 }
 
 
