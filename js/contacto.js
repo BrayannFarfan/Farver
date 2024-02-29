33982f68257da58d210ac2c25aeec84fb0e48e1b
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2024-02-28 22:22:09 -0300

    method for form

---
+++ js/contacto.js
@@ -0,0 +1,29 @@
+const D = document;
+
+
+
+const form = D.getElementById('contactForm');
+
+
+async function handleSubmitContact(e){
+    e.preventDefault();
+
+    const data = new FormData(this);
+
+    const res =await fetch(this.action, {
+        method: this.method,
+        body: data,
+        headers:{
+            'Accept':'application/json'
+        }
+    })
+
+    if(res.ok){
+        this.reset();
+        alert('Gracias por contartarme, te escribiré pronto')
+    }
+}
+
+
+form.addEventListener('submit', handleSubmitContact);
+
